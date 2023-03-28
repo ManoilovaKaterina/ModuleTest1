1fa742b9253131a7fa9ddf239bd77a8308d22f28
ManoilovaKaterina <katya.manoylova@gmail.com>
2023-03-28 13:18:55 +0300

    commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,10 @@
 import pytest
+from Main import GetDiff
+
+# @pytest.fixture()
+# def getFiles():
+
+
+@pytest.mark.parametrize("one, two", [([1, 1], [1, 2])])
+def test_diff(one, two):
+    assert GetDiff(one, two) == [2]
